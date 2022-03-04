5b6c19807dddc8793019712b55f77f5d906495f5
digvijay13873 <71278693+digvijay13873@users.noreply.github.com>
2022-03-04 16:04:10 +0530

    Update Table_Creation.py
    table creation 3.0

--- dags/Table_Creation.py
+++ dags/Table_Creation.py
@@ -20,35 +20,28 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag_psql = DAG(
+with DAG (
     dag_id = "Table_Creation",
     default_args=args,
     # schedule_interval='0 0 * * *',
     schedule_interval='@once',	
     dagrun_timeout=timedelta(minutes=60),
     description='Create Tables in Postgres',
-    start_date = airflow.utils.dates.days_ago(1)
-)
+    start_date = airflow.utils.dates.days_ago(1),
+    template_searchpath='/opt/airflow/fromlocal',
+    catchup=False
+) as dag :
 
 #Specify Queries For Postgres
-create_table_sql_query = """ 
-  CREATE TABLE IF NOT EXISTS tickers1 (ID SERIAL, keyword VARCHAR(255) NOT NULL, companyName VARCHAR(255));
-  CREATE TABLE IF NOT EXISTS stages (ID SERIAL, keyword VARCHAR(255) NOT NULL, twitterStage INTEGER DEFAULT 0, newsStage INTEGER DEFAULT 0, trendsStage INTEGER DEFAULT 0);
-  CREATE TABLE IF NOT EXISTS twitter (ID SERIAL, keyword VARCHAR(20), tweet TEXT, sentiment numeric(5,4));
-  CREATE TABLE IF NOT EXISTS trends (ID SERIAL, keyword VARCHAR(20), date DATE DEFAULT CURRENT_DATE, region VARCHAR(5), numberOfSearches NUMERIC(23,20));
-  CREATE TABLE IF NOT EXISTS news (ID SERIAL, keyword VARCHAR(20), news TEXT, sentiment numeric(5,4));
-  """
 
 #Specify Operator For Dag
 #postgres operator only works with sql commands
-create_table = PostgresOperator(
-      sql = create_table_sql_query,
-      task_id = "create_table_task",
-      postgres_conn_id = "postgres_config",
-      dag = dag_psql
-      )
+        create_table = PostgresOperator(
+        task_id='create_table',
+        postgres_conn_id='postgres_config',
+        sql='tables.sql',
+
+        )
 
 create_table
 
-if __name__ == "__main__":
-          dag_psql.cli()
